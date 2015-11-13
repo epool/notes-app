3bd338f5dd3f227fe09187d4fc864b426825355e
Eduardo Pool <epool@nearsoft.com>
2015-11-12 18:11:11 -0700

    - Support for tablets removed.

--- app/src/main/java/mx/eduardopool/notes/activities/NoteListActivity.java
+++ app/src/main/java/mx/eduardopool/notes/activities/NoteListActivity.java
@@ -40,12 +40,6 @@ import mx.eduardopool.notes.models.wrappers.NoteWrapper;
 public class NoteListActivity extends BaseActivity
         implements NoteListFragment.Callbacks, NoteDialogFragment.NoteDialogListener {
 
-    /**
-     * Whether or not the activity is in two-pane mode, i.e. running on a tablet
-     * device.
-     */
-    private boolean mTwoPane;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -59,19 +53,6 @@ public class NoteListActivity extends BaseActivity
                 noteDialogFragment.show(getSupportFragmentManager(), "NoteDialogFragment");
             }
         });
-
-        if (binding.frameLayout.findViewById(R.id.note_detail_container) != null) {
-            // The detail container view will be present only in the
-            // large-screen layouts (res/values-large and
-            // res/values-sw600dp). If this view is present, then the
-            // activity should be in two-pane mode.
-            mTwoPane = true;
-
-            // In two-pane mode, list items should be given the
-            // 'activated' state when touched.
-            NoteListFragment noteListFragment = (NoteListFragment) getSupportFragmentManager().findFragmentById(R.id.note_list);
-            noteListFragment.setActivateOnItemClick(true);
-        }
     }
 
     @Override
@@ -86,25 +67,11 @@ public class NoteListActivity extends BaseActivity
     @Override
     public void onItemSelected(Note note) {
         NoteWrapper noteWrapper = new NoteWrapper(note);
-        if (mTwoPane) {
-            // In two-pane mode, show the detail view in this activity by
-            // adding or replacing the detail fragment using a
-            // fragment transaction.
-            Bundle arguments = new Bundle();
-            arguments.putParcelable(NoteDetailFragment.ARG_NOTE_ITEM, noteWrapper);
-            NoteDetailFragment fragment = new NoteDetailFragment();
-            fragment.setArguments(arguments);
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.note_detail_container, fragment)
-                    .commit();
-
-        } else {
-            // In single-pane mode, simply start the detail activity
-            // for the selected item ID.
-            Intent detailIntent = new Intent(this, NoteDetailActivity.class);
-            detailIntent.putExtra(NoteDetailFragment.ARG_NOTE_ITEM, noteWrapper);
-            startActivity(detailIntent);
-        }
+        // In single-pane mode, simply start the detail activity
+        // for the selected item ID.
+        Intent detailIntent = new Intent(this, NoteDetailActivity.class);
+        detailIntent.putExtra(NoteDetailFragment.ARG_NOTE_ITEM, noteWrapper);
+        startActivity(detailIntent);
     }
 
     @Override
